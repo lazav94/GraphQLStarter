819520d3f345c9fead9778871fb1d2f32fbe8be8
Lazar <lazav94@gmail.com>
2020-05-07 15:01:04 +0200

    test husky

--- backend/lint-staged.config.js
+++ backend/lint-staged.config.js
@@ -3,6 +3,7 @@ module.export = {
     '**/*.+(js|md|ts|css|graphql|yml|yaml|json)': [
       'eslint --fix',
       'prettier --write',
+      'npm run oo',
       'jest --findRelatedTests',
       'git add',
     ],
